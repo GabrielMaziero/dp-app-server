64072d70844599451b67d6e2ea33eb586b121320
Gabriel Maziero <gabrielmaziero@MacBook-Air-de-Gabriel.local>
2022-02-07 09:17:45 -0300

    refactor: change call database in repository

--- src/repositories/implementations/MongoAccountRepository.ts
+++ src/repositories/implementations/MongoAccountRepository.ts
@@ -1,20 +1,29 @@
+import { MongoHelper } from "../../config/database";
 import { Account } from "../../entities/Account";
 import { AccountRepository } from "../protocols/accountRepository";
-import AccountMongo from './model/account';
+import { UpdateAccessTokenRepository } from "../protocols/updateAcessTokenRepository";
+import { ObjectId } from 'mongodb';
 
-export class MongoAccountRepository implements AccountRepository {
-  async save(account: Account): Promise<void> {
-    const accountMongo = await AccountMongo.create(account)
-    console.log(accountMongo)
-  }
 
-  async get(email: string): Promise<Account> {
-    const account = await AccountMongo.findOne({ email })
-    return account
+export class MongoAccountRepository implements AccountRepository, UpdateAccessTokenRepository {
+  async save(account: Account): Promise<string> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    return (await accountCollection.insertOne(account)).insertedId.toString()
   }
 
   async findByEmail(email: string): Promise<Account> {
-    const account = await AccountMongo.findOne({ email })
-    return account
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const account = await accountCollection.findOne({ email })
+    return account && MongoHelper.map(account)
   }
+
+  async updateAccessToken(id: string, token: string): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne({ _id: new ObjectId(id) }, {
+      $set: {
+        accessToken: token
+      }
+    })
+  }
+
 }
--- src/repositories/protocols/accountRepository.ts
+++ src/repositories/protocols/accountRepository.ts
@@ -2,6 +2,5 @@ import { Account } from "../../entities/Account";
 
 export interface AccountRepository {
   findByEmail(email: string): Promise<Account>;
-  save(account: Account): Promise<void>;
-  get(email: string): Promise<Account>;
+  save(account: Account): Promise<string>;
 }
