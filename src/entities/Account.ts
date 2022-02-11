84f60b768290b5836aac2b84d2d49f98b0aacc6c
Gabriel Maziero <gabrielmaziero@MacBook-Air-de-Gabriel.local>
2022-02-11 14:32:57 -0300

    refactor: remove password

--- src/entities/Account.ts
+++ src/entities/Account.ts
@@ -5,7 +5,6 @@ export class Account {
   public email: string;
   public gender?: string
   public birthday?: string
-  public password: string;
 
   constructor(props: Account) {
     Object.assign(this, props);
