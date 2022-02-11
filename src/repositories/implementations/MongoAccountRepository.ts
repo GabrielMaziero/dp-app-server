d92dbde524922e1ce505b89d3216073c4f8d4c37
Gabriel Maziero <gabrielmaziero@MacBook-Air-de-Gabriel.local>
2022-02-11 14:34:56 -0300

    refactor: return repository accountModel

--- src/repositories/implementations/MongoAccountRepository.ts
+++ src/repositories/implementations/MongoAccountRepository.ts
@@ -3,15 +3,17 @@ import { Account } from "../../entities/Account";
 import { AccountRepository } from "../protocols/accountRepository";
 import { UpdateAccessTokenRepository } from "../protocols/updateAcessTokenRepository";
 import { ObjectId } from 'mongodb';
+import { AccountModel } from "../interfaces/account-model";
 
 
 export class MongoAccountRepository implements AccountRepository, UpdateAccessTokenRepository {
-  async save(account: Account): Promise<string> {
+  async save(account: Account): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
-    return (await accountCollection.insertOne(account)).insertedId.toString()
+    const result = await accountCollection.insertOne(account)
+    return MongoHelper.map(result)
   }
 
-  async findByEmail(email: string): Promise<Account> {
+  async findByEmail(email: string): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
     const account = await accountCollection.findOne({ email })
     return account && MongoHelper.map(account)
--- src/repositories/protocols/accountRepository.ts
+++ src/repositories/protocols/accountRepository.ts
@@ -1,6 +1,7 @@
 import { Account } from "../../entities/Account";
+import { AccountModel } from "../interfaces/account-model";
 
 export interface AccountRepository {
-  findByEmail(email: string): Promise<Account>;
-  save(account: Account): Promise<string>;
+  findByEmail(email: string): Promise<AccountModel>;
+  save(account: Account): Promise<AccountModel>;
 }
