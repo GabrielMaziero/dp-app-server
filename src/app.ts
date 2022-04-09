0849aca0c8c49b8c07497345def2a1aa4cb0839c
Gabriel Maziero <gabrielmaziero@MacBook-Air-de-Gabriel.local>
2022-04-09 17:31:15 -0300

    refactor: remove passport

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,8 @@
 import express from 'express';
 import router from './routes';
-import passport from 'passport';
-import { PassportAuth } from './config/passport-auth';
 
 const app = express();
-const passportAuth = new PassportAuth()
 
-passport.use(passportAuth.googleStrategy())
-app.use(passport.initialize())
 app.use(express.json());
 app.use('/api/', router);
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,29 +1,8 @@
 import { Request, Response, Router } from "express";
-import passport from 'passport';
 import { createAccountController } from "./useCases/createAccount";
 import { loginAccountController } from "./useCases/loginAccount";
 
 const router = Router();
-
-router.get('/', (req: Request, res: Response) => {
-  const { user } = req;
-  return res.json({ user })
-})
-
-router.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}))
-
-router.get('/logout', (req: Request, res: Response) => {
-  req.logout();
-  res.redirect('/');
-});
-
-router.get('/oauth2/redirect/google',
-  passport.authenticate('google', { failureRedirect: '/', failureMessage: true }),
-  (req: Request, res: Response) => {
-    res.redirect('/api');
-  });
 
 router.post('/users', (req: Request, res: Response) => createAccountController.handle(req, res))
 
